909baaaabbdcc4d339b498ea8e54dc23e63e56af
Replit user <>
2023-04-20 23:07:53 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -79,7 +79,7 @@ Pair * searchTreeMap(TreeMap * tree, void* key) {
     if(resultado == 0){
       return tree->current->pair;
     }
-    else if(resultado < 0){
+    else if(resultado > 0){
       tree -> current = tree -> current -> left;
     }
     else{
