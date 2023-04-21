0452ecd773efc17d2a1dc53eba4aa1f36552807a
Replit user <>
2023-04-21 12:37:15 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -95,11 +95,11 @@ Pair * searchTreeMap(TreeMap * tree, void* key) {
       printf("%i", *((int*)(tree->current->pair->value)));
       return tree->current->pair;
     }
-    else if(tree->lower_than(key,tree->current->pair->key) == 0){
-      tree -> current = tree -> current -> left;
+    else if(tree->lower_than(key,tree->current->pair->key) == 1){
+      tree -> current = tree -> current -> right;
     }
     else{
-      tree -> current = tree -> current -> right;
+      tree -> current = tree -> current -> left;
     }
   }
   return NULL;
