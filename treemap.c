cac0ced4417a6e38ae80b4b0b796b621b9d72b6a
Replit user <>
2023-04-22 23:28:14 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -117,7 +117,7 @@ void removeNode(TreeMap * tree, TreeNode* node) {
     }
   }
   else{
-    TreeNode *min = minimum(node -> root);
+    TreeNode *min = minimum(node -> left);
     node -> pair -> key = min -> pair -> key;
     node -> pair -> value = min -> pair -> value;
     removeNode(tree,min);
